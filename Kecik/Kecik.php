a7c6253b431e6feb0ca6b33b56694531e580b81a
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-03-23 10:49:55 +0700

    Perbaikan MVC dan Autoload MVC

--- Kecik/Kecik.php
+++ Kecik/Kecik.php
@@ -43,31 +43,33 @@ namespace Kecik;
  * @author 		Dony Wahyu Isp
  * @since 		1.0-alpha
  **/
-class Controller {
+if (!class_exists('Controller')) {
+	class Controller {
 
-	/**
-	 * Construtor Controller
-	 **/
-	public function __construct() {
-		//Silakan tambah inisialisasi controller sendiri disini
+		/**
+		 * Construtor Controller
+		 **/
+		public function __construct() {
+			//Silakan tambah inisialisasi controller sendiri disini
 
-		//-- Akhir tambah inisialisasi sendiri
-	}
+			//-- Akhir tambah inisialisasi sendiri
+		}
 
-	//Silakan tambah fungsi controller sendiri disini
+		//Silakan tambah fungsi controller sendiri disini
 
 
-	//-- Akhir tambah fungsi sendiri
+		//-- Akhir tambah fungsi sendiri
 
-	/**
-	 * view
-	 * Funngsi untuk menampilkan view
-	 * @param string $file
-	 * @param array $param
-	 **/
-	protected function view($file, $param=array()) {
-		extract($param);
-		include Config::get('path.app').'/views/'.$file.'.php';
+		/**
+		 * view
+		 * Funngsi untuk menampilkan view
+		 * @param string $file
+		 * @param array $param
+		 **/
+		protected function view($file, $param=array()) {
+			extract($param);
+			include Config::get('path.app').'/views/'.$file.'.php';
+		}
 	}
 }
 
@@ -77,129 +79,131 @@ class Controller {
  * @author 		Dony Wahyu Isp
  * @since 		1.0-alpha1
  **/
-class Model {
-	protected $_field = array();
-	protected $_where;
-	protected $add = TRUE;
-	protected $table = '';
-	protected $fields = array();
-	protected $values = array();
-	protected $updateVar = array();
+if (!class_exists('Model')) {
+	class Model {
+		protected $_field = array();
+		protected $_where;
+		protected $add = TRUE;
+		protected $table = '';
+		protected $fields = array();
+		protected $values = array();
+		protected $updateVar = array();
 
-	/**
-	 * save
-	 * Fungsi untuk menambah atau mengupdate record (Insert/Update)
-	 * @return string SQL Query
-	 **/
-	public function save() {
-		$this->setFieldsValues();
+		/**
+		 * save
+		 * Fungsi untuk menambah atau mengupdate record (Insert/Update)
+		 * @return string SQL Query
+		 **/
+		public function save() {
+			$this->setFieldsValues();
 
-		if ($this->table != '') {
-			// Untuk menambah record
-			if ($this->add == TRUE) {
-				$sql ="INSERT INTO `$this->table` ($this->fields) VALUES ($this->values)";
-			// Untuk mengupdate record
-			} else {
-				$sql ="UPDATE `$this->table` SET $this->updateVar $this->_where";
-			}
-
-			//silakan tambah code database sendiri disini
-
-
-			//-- Akhir tambah code database sendiri
-		}
-
-		return (isset($sql))?$sql:'';
-	}
-
-	/**
-	 * delete
-	 * Fungsi untuk menghapus record
-	 * @return string SQL Query
-	 **/
-	public function delete() {
-		$this->setFieldsValues();
-
-		if ($this->table != '') {
-			if ($this->_where != '') {
-				$sql = "DELETE FROM $this->table $this->_where";
-			}
-
-			//silakan tambah code database sendiri disini
-
-
-			//-- AKhir tambah code database sendiri
-		}
-
-		return (isset($sql))?$sql:'';
-	}
-
-	//Silakan tambah fungsi model sendiri disini
-
-
-	//-- Akhir tambah fungsi sendiri
-
-
-	/**
-	 * Model Constructor
-	 * @param mixed $id
-	 **/
-	public function __construct($id='') {
-		$this->_where = '';
-		if ($id != '') {
-			if (is_array($id)) {
-				$and = array();
-				while(list($field, $value) = each($id)) {
-
-					if (preg_match('/<|>|!=/', $value))
-						$and[] = "`$field`$value";
-					else
-						$and[] = "`$field`='$value'";
+			if ($this->table != '') {
+				// Untuk menambah record
+				if ($this->add == TRUE) {
+					$sql ="INSERT INTO `$this->table` ($this->fields) VALUES ($this->values)";
+				// Untuk mengupdate record
+				} else {
+					$sql ="UPDATE `$this->table` SET $this->updateVar $this->_where";
 				}
-				$this->_where .= implode(' AND ', $and);
-			} else {
-				$this->_where .= "`id`='".$id."'";
+
+				//silakan tambah code database sendiri disini
+
+
+				//-- Akhir tambah code database sendiri
 			}
 
-			$this->add = FALSE;
-
-			//Silakan tambah inisialisasi model sendiri disini
-
-			//-- Akhir tambah inisialisasi model sendiri
+			return (isset($sql))?$sql:'';
 		}
-	}
 
-	/**
-	 * setFieldValues
-	 * Fungsi untuk menyetting Variable Fields dan Values
-	 **/
-	private function setFieldsValues() {
-		$fields = array_keys($this->_field);
-		while(list($id, $field) = each($fields))
-			$fields[$id] = "`$fields[$id]`";
-		
-		$this->fields = implode(',', $fields);
+		/**
+		 * delete
+		 * Fungsi untuk menghapus record
+		 * @return string SQL Query
+		 **/
+		public function delete() {
+			$this->setFieldsValues();
 
-		$values = array_values($this->_field);
-		$updateVar = array();
-		while (list($id, $value) = each($values)){
-			$values[$id] = "'$values[$id]'";
-			$updateVar[] = "$fields[$id] = $values[$id]";
+			if ($this->table != '') {
+				if ($this->_where != '') {
+					$sql = "DELETE FROM $this->table $this->_where";
+				}
+
+				//silakan tambah code database sendiri disini
+
+
+				//-- AKhir tambah code database sendiri
+			}
+
+			return (isset($sql))?$sql:'';
 		}
-		$this->values = implode(',', $values);
-		$this->updateVar = implode(',', $updateVar);
 
-		$this->_where = ($this->_where != '')?' WHERE '.$this->_where:'';
-	}
+		//Silakan tambah fungsi model sendiri disini
 
-	public function __set($var, $value) {
-		$this->_field[$var] = addslashes($value);
-	}
 
-	public function __get($var) {
-		return stripslashes($this->_field[$var]);
-	}
-} 
+		//-- Akhir tambah fungsi sendiri
+
+
+		/**
+		 * Model Constructor
+		 * @param mixed $id
+		 **/
+		public function __construct($id='') {
+			$this->_where = '';
+			if ($id != '') {
+				if (is_array($id)) {
+					$and = array();
+					while(list($field, $value) = each($id)) {
+
+						if (preg_match('/<|>|!=/', $value))
+							$and[] = "`$field`$value";
+						else
+							$and[] = "`$field`='$value'";
+					}
+					$this->_where .= implode(' AND ', $and);
+				} else {
+					$this->_where .= "`id`='".$id."'";
+				}
+
+				$this->add = FALSE;
+
+				//Silakan tambah inisialisasi model sendiri disini
+
+				//-- Akhir tambah inisialisasi model sendiri
+			}
+		}
+
+		/**
+		 * setFieldValues
+		 * Fungsi untuk menyetting Variable Fields dan Values
+		 **/
+		private function setFieldsValues() {
+			$fields = array_keys($this->_field);
+			while(list($id, $field) = each($fields))
+				$fields[$id] = "`$fields[$id]`";
+			
+			$this->fields = implode(',', $fields);
+
+			$values = array_values($this->_field);
+			$updateVar = array();
+			while (list($id, $value) = each($values)){
+				$values[$id] = "'$values[$id]'";
+				$updateVar[] = "$fields[$id] = $values[$id]";
+			}
+			$this->values = implode(',', $values);
+			$this->updateVar = implode(',', $updateVar);
+
+			$this->_where = ($this->_where != '')?' WHERE '.$this->_where:'';
+		}
+
+		public function __set($var, $value) {
+			$this->_field[$var] = addslashes($value);
+		}
+
+		public function __get($var) {
+			return stripslashes($this->_field[$var]);
+		}
+	} 
+}
 
 
 /**
@@ -830,7 +834,9 @@ class Kecik {
 	 **/
 	public function autoload($class) {
 		$class_array = explode('\\', $class);
-		include $this->config->get('path.app').'/'.strtolower($class_array[0]).'s/'.$class_array[1].'.php';
+		$file_load = $this->config->get('path.app').'/'.strtolower($class_array[0]).'s/'.$class_array[1].'.php';
+		if (file_exists($file_load))
+			include $file_load;
 	}
 
 	/**
