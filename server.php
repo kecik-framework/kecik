a9d07410ad064a746f499b5b1e655464a6df8e07
dnaextrim <dna.extrim@gmail.com>
2016-07-13 16:25:57 +0700

    fixing bug

--- server.php
+++ server.php
@@ -1,10 +1,7 @@
 <?php
-
-//echo "Elspranicode development server started on http://localhost:8000\n";
-//print "Elspranicode development server started on http://localhost:8000\n";
 fwrite(STDOUT, "Kecik development server started on http://localhost:8000\n");
 
-$cmd = "php -S localhost:8000 index.php";
+$cmd = "php -S localhost:8000 -t public";
 
 $output = shell_exec($cmd);
 
@@ -13,24 +10,4 @@ $output = shell_exec($cmd);
 } else {
     exec($cmd . " > /dev/null &");
 }*/
-//echo "Elspranicode development server started on http://localhost:8000\n";
-//print "Elspranicode development server started on http://localhost:8000\n";
-//fwrite(STDOUT, "Elspranicode development server started on http://localhost:8000\n");
-
-/*
-while (true) {
-	$post = $_POST;
-	$get = $_GET;
-
-	if ($post)
-		print_r($post);
-
-	if ($get)
-		print_r($get);
-
-
-	if ($_SERVER['HTTP_REFERER'])
-		print_r($_SERVER['HTTP_REFERER']);
-
-}
-*/
+// fwrite(STDOUT, "Kecik development server started on http://localhost:8000\n");
