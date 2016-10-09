fd19594e65c973a198eb71de5171e133716cd0a5
dnaextrim <dna.extrim@gmail.com>
2016-10-09 11:52:25 +0700

    Update for Version 2.0

--- example/index.php
+++ example/index.php
@@ -5,93 +5,104 @@
 //require_once "Kecik.php";
 require_once "../Kecik/Kecik.php";
 
+use Kecik\Assets;
+use Kecik\Config;
+use Kecik\Kecik;
+use Kecik\Route;
+
+
 $config = array(
     'path.basepath' => __DIR__ . '/'
 );
 
-$app = new \Kecik\Kecik();
+Kecik::run(function() {
+    Config::apply(function() {
+        $this->set('path.basepath', __DIR__ . '/');
+        $this->set('path.assets', 'assets');
+        $this->set('path.mvc', 'app');
+        $this->set('path.template', 'templates');
+        $this->set('error.404', '404');
+    });
+    
+    Assets::$css->add('bootstrap.min');
+    Assets::$css->add('bootstrap-theme.min');
+    Assets::$css->add('starter-template');
+    Assets::$js->add('jquery.min');
+    Assets::$js->add('bootstrap.min');
+    
+    $dbcon = null;
+    $this->before(function() use(&$dbcon) {
+        $dbcon = @mysqli_connect(
+            'localhost',
+            'root',
+            '',
+            'kecik'
+        );
+    
+        if (mysqli_connect_errno($dbcon)) {
+            header('X-Error-Message: Fail Connecting', true, 500);
+            die("Failed to connect to MySQL: " . mysqli_connect_error());
+        }
+    });
+    
+    Route::get(
+        '/',
+        function () use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            return $controller->index();
+        }
+    )->template('template_kecik');
+    
+    Route::get(
+        'data',
+        function () use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            return $controller->Data();
+        }
+    )->template('template_kecik');
+    
+    Route::get(
+        'tambah',
+        function () use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            return $controller->Form();
+        }
+    )->template('template_kecik');
+    
+    Route::get(
+        'edit/:id',
+        function ($id) use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            return $controller->Form($id);
+        }
+    )->template('template_kecik');
+    
+    Route::get(
+        'delete/:id',
+        function ($id) use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            $controller->delete($id);
+        }
+    );
+    
+    Route::post(
+        'save',
+        function () use ($dbcon) {
+            $controller = new Controller\Welcome($this, $dbcon);
+            $controller->save();
+        }
+    );
+    
+    Route::post(
+        'update/:id',
+        function ($id) use ($dbcon) {
+            $controller = new Controller\Welcome($dbcon);
+            $controller->update($id);
+        }
+    );
+    
+});
 
-$app->config->set('path.assets', 'assets');
-$app->config->set('path.mvc', 'app');
-$app->config->set('path.template', 'templates');
-$app->config->set('error.404', '404');
 
-$app->assets->css->add('bootstrap.min');
-$app->assets->css->add('bootstrap-theme.min');
-$app->assets->css->add('starter-template');
-$app->assets->js->add('jquery.min');
-$app->assets->js->add('bootstrap.min');
-
-$dbcon = @mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'kecik'
-);
-
-if (mysqli_connect_errno($dbcon)) {
-    header('X-Error-Message: Fail Connecting', true, 500);
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-
-
-$app->get(
-    '/',
-    function () use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        return $controller->index();
-    }
-)->template('template_kecik');
-
-$app->get(
-    'data',
-    function () use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        return $controller->Data();
-    }
-)->template('template_kecik');
-
-$app->get(
-    'tambah',
-    function () use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        return $controller->Form();
-    }
-)->template('template_kecik');
-
-$app->get(
-    'edit/:id',
-    function ($id) use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        return $controller->Form($id);
-    }
-)->template('template_kecik');
-
-$app->get(
-    'delete/:id',
-    function ($id) use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        $controller->delete($id);
-    }
-);
-
-$app->post(
-    'save',
-    function () use ($dbcon) {
-        $controller = new Controller\Welcome($this, $dbcon);
-        $controller->save();
-    }
-);
-
-$app->post(
-    'update/:id',
-    function ($id) use ($dbcon) {
-        $controller = new Controller\Welcome($dbcon);
-        $controller->update($id);
-    }
-);
-
-
-$app->run();
 
 mysqli_close($dbcon);
